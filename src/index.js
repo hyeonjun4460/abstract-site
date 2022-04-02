4b9e3bead8f3a3b5584182542d88bf1fbe28bf9f
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-04-02 23:37:42 +0900

    개인과제 순한맛 풀이 완료

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 class Site {
     constructor() {
-        this.boards = [];
+        this.boards = []
+        this.board = this.boards.find((element) => element)
     }
 
     addBoard(newboard) {
         if (this.boards.find((element) => element.name === newboard.name)) {
-            throw new Error({ message: 'error' });
+            throw new Error
         } else {
-            this.boards.push(newboard);
+            this.boards.push(newboard)
+            newboard.belongInSite = true
         }
     }
 
@@ -18,13 +20,79 @@ class Site {
 
 class Board {
     constructor(name) {
-        this.name = name;
+        if (name === null || name === '') {
+            throw new Error
+        }
+        else {
+            this.belongInSite = false
+            this.name = name;
+            this.article = []
+        }
+    }
+
+    publish(article) {
+        if (this.belongInSite === false) {
+            throw new Error
+        } else {
+            this.article.push(article)
+            article.id = this.name + '-'
+            article.belongInBoard = true
+        }
+    }
+
+    getAllArticles() {
+        return this.article
     }
 }
 
-class Article { }
+class Article {
+    constructor({ subject, content, author }) {
+        if (subject === null || subject === '' ||
+            content === null || content === '' ||
+            author === null || author === '') {
+            throw new Error
+        } else {
+            this.subject = subject,
+                this.content = content,
+                this.author = author,
+                this.id = '',
+                this.createdDate = new Date().toISOString(),
+                this.belongInBoard = false,
+                this.comment = []
+        }
+    }
 
-class Comment { }
+    startsWith(id) {
+        if (id === this.id) {
+            return true
+        }
+    }
+
+    reply(comment) {
+        if (this.belongInBoard === false) {
+            throw new Error
+        } else {
+            this.comment.push(comment)
+        }
+    }
+
+    getAllComments() {
+        return this.comment
+    }
+}
+
+class Comment {
+    constructor({ content, author }) {
+        if (content === 'null' || content === '' ||
+            author === 'null' || author === '') {
+            throw new Error
+        } else {
+            this.content = content,
+                this.author = author,
+                this.createdDate = new Date().toISOString()
+        }
+    }
+}
 
 module.exports = {
     Site,
